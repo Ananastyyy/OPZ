003a488e5462547d0718bdb7233265e6ba284721
Ananasty <novinast11112003@gmail.com>
2022-09-27 20:25:21 +0500

    Add interface

--- src/main/java/org/example/ReversePolishNotation.java
+++ src/main/java/org/example/ReversePolishNotation.java
@@ -6,18 +6,23 @@ import java.io.InputStreamReader;
 import java.io.BufferedReader;
 
 /**
- * Класс обратной польской записи с методами readLine и getResult.
+ * Вычисление выражения, записанного в виде обратной польской нотации
  *
  * @author Новикова Анастасия
  */
-public class ReversePolishNotation {
+
+/**
+ * Абстрактный класс, связанный с интерфейсом Notation
+ */
+public class ReversePolishNotation implements Notation {
 
     /**
      * Метод, осуществляющий считывание записи с консоли.
      *
+     * @return строка с выражением, а при ошибке вызывается исключение
      * @throws IOException вызывается, если ввод не был осуществлен
      */
-    public static String readLine() {
+    public String readLine() {
         try {
             return new BufferedReader(new InputStreamReader(System.in)).readLine();
         } catch (IOException e) {
@@ -33,24 +38,24 @@ public class ReversePolishNotation {
      * @throws ArithmeticException вызывается, если произошло деление на 0
      * @throws Exception           если произошел неверный ввод строки
      */
-    public static Double getResult(String in) throws Exception {
+    public Double getResult(String in) throws Exception {
         char[] arr = in.toCharArray();
-        Stack<Double> exp = new Stack();
+        Stack<Double> expression = new Stack();
         for (char c : arr) {
             if (Character.isDigit(c)) {
-                exp.push(Double.parseDouble(String.valueOf(c)));
-            } else if (exp.size() >= 2) {
-                Double a = exp.pop();
-                Double b = exp.pop();
+                expression.push(Double.parseDouble(String.valueOf(c)));
+            } else if (expression.size() >= 2) {
+                Double a = expression.pop();
+                Double b = expression.pop();
                 switch (c) {
-                    case '+' -> exp.push(a + b);
-                    case '-' -> exp.push(b - a);
-                    case '*' -> exp.push(b * a);
+                    case '+' -> expression.push(a + b);
+                    case '-' -> expression.push(b - a);
+                    case '*' -> expression.push(b * a);
                     case '/' -> {
                         if (a == 0) {
                             throw new ArithmeticException("Division by zero");
                         } else {
-                            exp.push(b / a);
+                            expression.push(b / a);
                         }
                     }
                 }
@@ -58,15 +63,15 @@ public class ReversePolishNotation {
                 throw new Exception("Wrong line!");
             }
         }
-        if (exp.size() >= 2) {
+        if (expression.size() >= 2) {
             throw new Exception("Wrong line!");
         }
-        return exp.pop();
+        return expression.pop();
     }
 
     public static void main(String[] args) throws Exception {
-        String in = readLine();
-        Double result = getResult(in);
-        System.out.println(result);
+        ReversePolishNotation notation = new ReversePolishNotation();
+        String input = notation.readLine();
+        System.out.println(notation.getResult(input));
     }
 }
--- src/test/java/org/example/ReversePolishNotationTest.java
+++ src/test/java/org/example/ReversePolishNotationTest.java
@@ -4,32 +4,36 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class ReversePolishNotationTest {
+class ReversePolishNotationTest implements Notation {
 
     @Test
     void testCorrect() throws Exception {
-        Double res = ReversePolishNotation.getResult("825*+132*+4-/");
+        ReversePolishNotation notation = new ReversePolishNotation();
+        Double res = notation.getResult("825*+132*+4-/");
         assertEquals(6, res);
     }
 
     @Test
     void testMuchNumber() throws Exception {
+        ReversePolishNotation notation = new ReversePolishNotation();
         assertThrows(Exception.class, () -> {
-            Double res = ReversePolishNotation.getResult("532++73");
+            Double res = notation.getResult("532++73");
         });
     }
 
     @Test
     void testDivisionByZero() throws Exception {
+        ReversePolishNotation notation = new ReversePolishNotation();
         assertThrows(Exception.class, () -> {
-            Double res = ReversePolishNotation.getResult("825*+132*+7-/");
+            Double res = notation.getResult("825*+132*+7-/");
         });
     }
 
     @Test
     void testEmptyStack() throws Exception {
+        ReversePolishNotation notation = new ReversePolishNotation();
         assertThrows(Exception.class, () -> {
-            Double res = ReversePolishNotation.getResult("532++73++++");
+            Double res = notation.getResult("532++73++++");
         });
     }
 }
